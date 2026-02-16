1748a41c7efa6cdb4d6515a6ea2cb3354ab5d702
Saraansh <gandhisaraansh@gmail.com>
2026-02-16 12:45:20 +0530

    Implement majority element 2 using Moore’s Voting Algo

--- leetcode229-2.java
+++ leetcode229-2.java
@@ -1,1 +1,42 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
 
+        int c1=0 , c2=0;
+        int v1=0 , v2=0;
+
+        for ( int num : nums ){
+            
+            if ( v1 == num ){
+                c1++;
+            }else if ( v2 == num){
+                c2++;
+            }
+            else if ( c1==0 ){
+                v1 = num ;
+                c1=1;
+            } else if ( c2==0 ){
+                v2 = num ;
+                c2=1;
+            } else {
+                c1-- ;
+                c2--;
+            }
+        }
+
+        c1 = 0;
+        c2 = 0;
+        
+        for (int num : nums) {
+            if (num == v1) c1++;
+            else if (num == v2) c2++;
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        if (c1 > nums.length / 3) result.add(v1);
+        if (c2 > nums.length / 3) result.add(v2);
+        
+        return result;
+
+    }
+}
