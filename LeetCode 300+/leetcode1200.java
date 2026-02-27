1839a2c0b565c39ccbcdd13f9c0d27d643730bcf
Saraansh <gandhisaraansh@gmail.com>
2026-02-28 00:53:11 +0530

    leetcode 1200 done

---
+++ LeetCode 300+/leetcode1200.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        Arrays.sort(arr);
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff == minDiff) {
+                List<Integer> pair = new ArrayList<>();
+                pair.add(arr[i]);
+                pair.add(arr[i + 1]);
+                result.add(pair);
+            }
+        }
+
+        return result;
+    }
+}
