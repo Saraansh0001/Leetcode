4f69e4644b50c63e520ef0a9d457e50e6a71f564
Saraansh <gandhi.saraansh@gmail.com>
2026-03-02 12:27:48 +0530

    2nd way of leetcode 162 using variable

---
+++ leetcode 100-200/leetcode162-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+
+        int n = arr.length;
+
+        int minIndex = -1 ;
+
+        if (n == 1) return 0;
+
+        if (arr[0] > arr[1]){
+            minIndex = 0;
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) 
+                minIndex = i;
+        }
+
+        if (arr[n - 1] > arr[n-2]) 
+            minIndex =  n - 1;
+
+        if ( minIndex == -1 )
+            return -1 ;
+
+        return minIndex;
+    }
+}
