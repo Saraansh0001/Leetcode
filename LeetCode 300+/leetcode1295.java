73d5988643d6024bb82dee65fb6740818ed606db
Saraansh <gandhisaraansh@gmail.com>
2026-03-02 12:45:29 +0530

    1295 done

---
+++ LeetCode 300+/leetcode1295.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+
+        int n = nums.length -1 ;
+        int evenNumber = 0 ;
+        
+        for ( int i = 0 ; i <= n ; i ++){
+            int count = 0 ;
+
+            while( nums[i] > 0 ){
+                nums[i] = nums[i] / 10 ;
+                count ++;
+            }
+
+            if ( count % 2 == 0 ) evenNumber++ ;
+
+        }
+
+        return evenNumber ; 
+        
+    }
+}
