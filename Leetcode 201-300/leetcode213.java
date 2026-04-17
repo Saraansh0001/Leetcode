7d3ffbead04ca1cc5a3aaa165cd876dedbdc3a8b
Saraansh <gandhisaraansh@gmail.com>
2026-04-17 19:17:21 +0530

    house robber || using rec , tle occur

--- Leetcode 201-300/leetcode213.java
+++ Leetcode 201-300/leetcode213.java
@@ -1,1 +1,28 @@
 using recusion 
+
+class Solution {
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return nums[0];
+
+        int case1 = solve(nums, 0, n - 2);
+        int case2 = solve(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+
+    public int solve(int[] nums, int i, int end) {
+        // Base case
+        if (i > end) return 0;
+
+        // Rob
+        int rob = nums[i] + solve(nums, i + 2, end);
+
+        // Skip
+        int skip = solve(nums, i + 1, end);
+
+        return Math.max(rob, skip);
+    }
+}
