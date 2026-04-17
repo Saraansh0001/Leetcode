f6b45013ea36c5a2d8fa5d968b337febd6702691
Saraansh <gandhisaraansh@gmail.com>
2026-04-17 18:58:19 +0530

    l98 done , tle got next step would be dp

--- Leetcode 101-200/leetcode198.java
+++ Leetcode 101-200/leetcode198.java
@@ -1,1 +1,22 @@
-// using recurssion 
+using recurssion 
+
+class Solution {
+    
+    public int rob(int[] nums) {
+        return solve(nums, 0);
+    }
+
+    public int solve(int[] nums, int i) {
+        if (i >= nums.length) {
+            return 0;
+        }
+
+        int rob = nums[i] + solve(nums, i + 2);
+
+        int skip = solve(nums, i + 1);
+
+        return Math.max(rob, skip);
+    }
+}
+
+tle error as recursion is taking too much time for large input.
