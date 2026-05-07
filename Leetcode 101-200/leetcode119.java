27bb12e5d86ce4987bfa6f51e91dfdbd99034de0
Saraansh <gandhisaraansh@gmail.com>
2026-05-07 21:13:33 +0530

    Update leetcode119.java

--- Leetcode 101-200/leetcode119.java
+++ Leetcode 101-200/leetcode119.java
@@ -14,7 +14,7 @@ class Solution {
             ans.add(list);
         }
 
-        // Build Pascal's Triangle
+        // Build Pascal's Triangle ...
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
